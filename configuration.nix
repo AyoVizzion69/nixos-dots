a8a8bb12b97006120674d56dc900e48f244d9cd7
vizzion <AyoVizzion@protonmail.com>
2026-06-23 12:49:01 -0500

    Add files via upload

---
+++ configuration.nix
@@ -0,0 +1,86 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ 
+      /etc/nixos/hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "America/Chicago";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+  
+  # Window Manager
+  programs.sway.enable = true; 
+
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users."vizzion" = {
+    isNormalUser = true;
+    description = "vizzion";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  swayfx
+  neovim
+  git
+  pfetch
+  tealdeer
+  bat
+  ghostty
+  cmatrix
+  firefox
+  fuzzel
+  waybar
+  xwayland-satellite
+  thunar
+  mako
+  autotiling
+  ];
+
+fonts.packages = with pkgs; [
+  nerd-fonts.jetbrains-mono
+];
+
+nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  services = {
+  desktopManager.plasma6.enable = false;
+  displayManager.ly.enable = true;
+  openssh.enable = true;
+ };
+  
+
+  system.stateVersion = "26.05";
+}
