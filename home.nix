163cf82288b1b378146a27891d6347529ca16f2f
vizzion <AyoVizzion@protonmail.com>
2026-07-09 21:56:36 -0500

    config dwm

--- home.nix
+++ home.nix
@@ -18,6 +18,7 @@
   home.file.".config/fastfetch".source = ./config/fastfetch;
   home.file.".config/kitty".source = ./config/kitty;
   home.file.".config/rofi".source = ./config/rofi;
+  home.file.".config/dwm".source = ./config/dwm;
   home.packages = with pkgs; [
     fastfetch
     neovim
